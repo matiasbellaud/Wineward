580f1ae68e4f4ecb5e05bea1865fd4b1a4b9bfe8
matiasbellaud <bellaud.matias@gmail.com>
2024-03-15 12:04:58 +0100

    :sparkles: | profil page creation

--- src/views/AccessoryView/AccessoryView.tsx
+++ src/views/AccessoryView/AccessoryView.tsx
@@ -10,30 +10,27 @@ import {
   Image
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
 const  AccessoryView = () => {
 
 
   return (
-    <View>
-      {/* NAVIGATION BAR */}
-      <View style = {styles.navBar}>
-        <Image source={require('../../assets/images/MapIcon.png')} style={styles.navBarImage} /> 
-        <Text style={styles.navBarTitle}>Autour du vin</Text>
+    <View style = {styles.body}>
+      {/* HEADER */}
+      <View style = {styles.Header}>
+        <Image source={require('../../assets/images/MapIcon.png')} style={styles.HeaderImage} /> 
+        <Text style={styles.HeaderTitle}>Autour du vin</Text>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  navBar: {
+  body: {
+    backgroundColor:"#444444",
+    height:2000,
+  },
+
+  Header: {
     flexDirection: 'row',
     backgroundColor: "#AC1E44",
     height: 105,
@@ -41,14 +38,15 @@ const styles = StyleSheet.create({
     borderBottomRightRadius: 20,
     borderBottomLeftRadius: 20,
   },
-  navBarTitle: {
+  HeaderTitle: {
     fontSize:35,
     color:"#ffffff",
     width:200,
     marginLeft:0,
-    textAlign: 'center'
+    textAlign: 'center',
+    fontFamily:'LT Afficher Neue Text'
   },
-  navBarImage: {
+  HeaderImage: {
     width: 43, height: 43,
     left:-4,
     marginLeft:39,
--- src/views/MapView/MapView.tsx
+++ src/views/MapView/MapView.tsx
@@ -11,14 +11,6 @@ import {
   Image
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
 const  MapView = () => {
 
   // const [isLoaded] = useFonts({
@@ -26,36 +18,41 @@ const  MapView = () => {
   // });
 
   return (
-    <View>
-      {/* NAVIGATION BAR */}
-      <View style = {styles.navBar}>
-        <Image source={require('../../assets/images/MapIcon.png')} style={styles.navBarImage} /> 
-        <Text style={styles.navBarTitle}>Ma Carte</Text>
+    <View style = {styles.body}>
+      {/* HEADER */}
+      <View style = {styles.Header}>
+        <Image source={require('../../assets/images/MapIcon.png')} style={styles.HeaderImage} /> 
+        <Text style={styles.HeaderTitle}>Ma Carte</Text>
       </View>
   </View>
   );
 }
 
 const styles = StyleSheet.create({
-    navBar: {
-      flexDirection: 'row',
-      backgroundColor: "#AC1E44",
-      height: 105,
-      alignItems: "center",
-      borderBottomRightRadius: 20,
-      borderBottomLeftRadius: 20,
-    },
-    navBarTitle: {
-      fontSize:35,
-      color:"#ffffff",
-      // backgroundColor:"blue",
-      marginLeft:22,
-    },
-    navBarImage: {
-      width: 43, height: 43,
-      left:-4,
-      marginLeft:50,
-    }
+  body: {
+    backgroundColor:"#444444",
+    height:2000,
+  },
+
+  Header: {
+    flexDirection: 'row',
+    backgroundColor: "#AC1E44",
+    height: 105,
+    alignItems: "center",
+    borderBottomRightRadius: 20,
+    borderBottomLeftRadius: 20,
+  },
+  HeaderTitle: {
+    fontSize:35,
+    color:"#ffffff",
+    fontFamily:'LT Afficher Neue Text',
+    marginLeft:22,
+  },
+  HeaderImage: {
+    width: 43, height: 43,
+    left:-4,
+    marginLeft:50,
+  }
 });
 
 export default MapView;
